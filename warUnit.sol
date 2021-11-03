6466e3806647a8f47126c889a8aaca05c4aae6e8
utgak <degtyaryov.2002@mail.ru>
2021-11-03 06:34:03 +0300

    refactor

--- warUnit.sol
+++ warUnit.sol
@@ -5,12 +5,18 @@ import 'gameObjectInterface.sol';
 contract WarUnit is GameObjectInterface {
     int8 power;
 
+    modifier onlyOwner {
+        require(msg.pubkey() == tvm.pubkey(), 1337);
+		tvm.accept();
+		_;
+	}
+
     function attack(WarUnit object) public onlyOwner view {
         tvm.accept();
         object.takeTheAttack(power);
     }
 
-    function takeTheAttack(int8 _power) external override virtual {
+    function takeTheAttack(int8 _power) external override {
         tvm.accept();
 
         int8 damage = _power - defence;
@@ -32,22 +38,16 @@ contract WarUnit is GameObjectInterface {
         int8 public lives;
     int8 defence;
 
-    modifier onlyOwner {
-        require(msg.pubkey() == tvm.pubkey(), 1337);
-		tvm.accept();
-		_;
-	}
-
-    function getDefence() public returns (int8) {
+    function getDefence() public view returns (int8) {
         tvm.accept();
         return defence;
     }
 
-    function checkForDeath() internal view returns(bool) {
+    function checkForDeath() private view returns(bool) {
         return lives <= 0 ? true : false;
     }
 
-    function die(address enemyAddress) internal virtual {
+    function die(address enemyAddress) private {
         tvm.accept();
         selfdestruct(enemyAddress);
     }
